d0300dc69189ea0601907324ea364e7698ef7560
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-08-18 00:57:38 +0530

    add(API): initial setup

---
+++ src/test/java/com/blogging/api/BloggingPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogging.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingPlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
