88b346c8843e9322f4a5507411a27b3884af6b03
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-08-22 01:44:17 +0530

    Update V1__init.sql

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -3,10 +3,11 @@ CREATE TABLE `t_blogs`
     `id`          bigint(20) NOT NULL AUTO_INCREMENT,
     `title`       varchar(255) DEFAULT NULL,
     `content`     text,
+    `category`     varchar(255) DEFAULT NULL,
+    `tags`       varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
     `created_at`  datetime DEFAULT NULL,
     `updated_at`  datetime DEFAULT NULL,
-     `author`     varchar(255) DEFAULT NULL,
-     `tags`       varchar(255) DEFAULT NULL,
+
     PRIMARY KEY (`id`)
 )
 
